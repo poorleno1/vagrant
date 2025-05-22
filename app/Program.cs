c4d975544f569de1697fc4424afee1707af69d41
JarekO <jarekole@gmail.com>
2025-05-22 16:44:55 +0200

    Add files via upload

---
+++ app/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Azure.Cosmos;
+
+
+using CosmosClient client = new(
+    accountEndpoint: "https://localhost:8081/",
+    authKeyOrResourceToken: "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="
+);
+
+Database database = await client.CreateDatabaseIfNotExistsAsync(
+    id: "cosmicworks",
+    throughput: 400
+);
+
+Container container = await database.CreateContainerIfNotExistsAsync(
+    id: "products",
+    partitionKeyPath: "/id"
+);
+
+var item = new
+{
+    id = "68719518371",
+    name = "Kiama classic surfboard"
+};
+
+await container.UpsertItemAsync(item);
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
---
+++ app2/CosmosDbTest/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+var context = 
+    new ServiceCollection()
+        .AddCosmos<CosmosDbContext>(
+            connectionString: "AccountEndpoint=https://192.168.0.237:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==",
+            databaseName: "CosmosDb")
+        .BuildServiceProvider()
+        .GetRequiredService<CosmosDbContext>();
+
+await context.Database.EnsureCreatedAsync();
+
+context.Entity.Add(new Entity(Guid.NewGuid().ToString()));
+await context.SaveChangesAsync();
+
+class CosmosDbContext : DbContext
+{
+    public CosmosDbContext(DbContextOptions<CosmosDbContext> options) 
+        : base(options) { }
+
+    public DbSet<Entity> Entity { get; set; } = null!;
+}
+
+public record Entity(string Id);
